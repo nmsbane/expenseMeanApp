52815c0f76b4258eb30f983d49f6996213812d17
Madhusudan <madhusudhan638@gmail.com>
2016-05-17 13:00:12 +0000

    start with the project, create some boilerplate code and create a transaction page

---
+++ README.md
@@ -0,0 +1,12 @@
+# Expense Application
+
+* Add landing page
+* Add a page which shows all the transactions done by the user
+
+
+# Each transaction consists of the following details
+
+* Tag
+* Description (less than 30 chars)
+* Amount 
+* Date and time
---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+
+app.set('view engine', 'ejs');
+
+// landing page
+// app.get('/', function(req, res) {
+//     res.render('landing');
+// });
+
+
+app.get('/transactions', function(req, res) {
+    var listOfTransactions = [
+        {'tag': "Dinner", 'description': 'Dinner bill', 'amount': 100, 'date': 'some date here'},
+        {'tag': "lunch", 'description': 'lunch bill', 'amount': 100, 'date': 'some date here'},
+        {'tag': "snacks", 'description': 'snacks bill', 'amount': 100, 'date': 'some date here'},
+    ];
+    
+    res.render('transactions', {'transactions': listOfTransactions});
+})
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Expense App has started");
+});
